6ea968d8ceada62fe1c3e0722a884718e98d6b4c
Alan Shenoy <alanshenoy@gmail.com>
2025-06-19 20:57:16 -0400

    created a main python page

--- secuSave.py
+++ secuSave.py
@@ -1,4 +1,4 @@
-class BankSave:
+class bankSave:
     def __init__(self, customer_name, current_balance, minimum_balance):
         self.name = customer_name
         self.current_balance = current_balance
@@ -25,6 +25,24 @@ class BankSave:
         print(f"Current Balance: {self.current_balance}")
 
 print("Welcome to SecuSave credit union!!")
-my_account= BankSave("Alan", 1000, 500)
-my_account.deposit()
-my_account.withdraw()
+my_account= bankSave("Alan", 1000, 500)
+
+class savAcc(bankSave):
+
+    def interestFee(self):
+        self.current_balance *= 1.02 # 2% interest example
+
+class chekAcc(bankSave):
+    # class that imposes limits on just how much $ can be transferred
+    def transfer(self, targetAcc):
+        limit = 650
+        amount = float(input("How much would you like to transfer? "))
+
+        if amount > limit:
+            print("Transfer exceeds allotted limit!")
+        elif self.current_balance - amount < self.minimum_balance:
+            print("Insufficient funds!")
+        else:
+            self.current_balance -= amount
+            targetAcc.current_balance += amount
+            print(f"Transferred ${amount} to ${targetAcc.name}")
